9231bc5b7639d3bfd556463d4788f56d00184d78
JOAOPEDROTADEUCOSTA <joaopedrotadeucosta36@gmail.com>
2025-09-24 03:34:08 -0300

    flask adicionado
    flask adicionado sem nenhum teste

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+import datebase
+
+app = Flask(__name__)
+
+database.init_db()
+
+@app.route("/")
+def index():
+    produtos = database.get_all_produtos()
+    return render_template("index.html", produtos=produtos)
+
+@app.route("/adicionar", methods=["POST"])
+def adicionar():
+    tipo = request.form["tipo"]
+    nome = request.form["nome"]
+    quantidade = int(request.form["quantidade"])
+    preco = float(request.form["preco"])
+
+    database.add_produto(tipo, nome, quantidade, preco)
+    return redirect(url_for("index"))
+
+@app.route("/remover/<int:id>")
+def remover(id):
+    database.remove_produto(id)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
